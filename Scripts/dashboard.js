e1511e4071e91065c4e4b9b0aa983dbc9d452c59
James <James@LAPTOP-J6KSPLAD>
2023-06-23 09:11:19 +0800

    Add project files.

---
+++ Scripts/dashboard.js
@@ -0,0 +1,7 @@
+﻿const sidebar = document.getElementById('sidebar');
+const content = document.getElementById('content');
+
+function toggleSidebar() {
+    sidebar.classList.toggle('sidebar-hidden');
+    content.classList.toggle('content-full-width');
+}
